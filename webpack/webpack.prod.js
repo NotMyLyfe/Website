6646d309b433d6e139ceb7016a86c423c81d69f6
Gordon Lin <contact@gordonlin.ca>
2026-05-12 18:10:51 -0400

    Copy hero.jpg to build root for og:image meta tag w copy-webpack-plugin

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,4 +1,6 @@
 // @ts-check
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 /**
  * @type {import('webpack').Configuration}
@@ -6,4 +8,14 @@
 module.exports = {
     mode: 'production',
     devtool: 'source-map',
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, '..', './src/pics/hero.jpg'),
+                    to: 'hero.jpg',
+                },
+            ],
+        }),
+    ],
 };
